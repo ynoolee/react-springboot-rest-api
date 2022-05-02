912d1737c3c5382fb08cf4819eff6dd376cefb25
ynoolee <heerarea129@gmail.com>
2022-05-02 20:28:40 +0900

    Add : ProductController 구현, Product list 페이지 및 새로운 Product 추가 페이지 구현

---
+++ gc-coffee/src/main/java/com/prgrms/gccoffee/controller/CreateProductRequest.java
@@ -0,0 +1,7 @@
+package com.prgrms.gccoffee.controller;
+
+import com.prgrms.gccoffee.model.Category;
+
+public record CreateProductRequest(String productName, Category category, long price, String description) {
+
+}
---
+++ gc-coffee/src/main/java/com/prgrms/gccoffee/controller/ProductController.java
@@ -0,0 +1,42 @@
+package com.prgrms.gccoffee.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.prgrms.gccoffee.model.Product;
+import com.prgrms.gccoffee.service.ProductService;
+
+@Controller
+public class ProductController {
+
+	private final ProductService productService;
+
+	public ProductController(ProductService productService) {
+		this.productService = productService;
+	}
+
+	@GetMapping("/products")
+	public String productsPage(Model model) {
+		List<Product> products = productService.getAllProducts();
+		model.addAttribute("products", products);
+
+		return "product-list";
+	}
+
+	@GetMapping("new-product")
+	public String newProductPage() {
+		return "new-product";
+	}
+
+	@PostMapping("/products")
+	public String newProduct(CreateProductRequest createProductRequest) {
+		Product product = productService.createProduct(createProductRequest.productName(),
+			createProductRequest.category(), createProductRequest.price(), createProductRequest.description());
+
+		return "redirect:/products";
+	}
+}
