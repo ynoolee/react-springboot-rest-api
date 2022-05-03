19d4557130d02acd9fd461b2198c2d0a3e4e758d
ynoolee <heerarea129@gmail.com>
2022-05-03 17:43:28 +0900

    Feat: Orders 와 OrderItems 에 대한 DB 스키마 정의

--- gc-coffee/src/main/resources/schema.sql
+++ gc-coffee/src/main/resources/schema.sql
@@ -1,12 +1,40 @@
+drop table if exists order_items;
 drop table if exists products;
+drop table if exists orders;
 
 CREATE TABLE products
 (
-        product_id BINARY(16) PRIMARY KEY,
-        product_name VARCHAR(20) NOT NULL,
-        category VARCHAR(50) NOT NULL,
-        price BIGINT NOT NULL,
-        description VARCHAR(500) DEFAULT NULL,
-        created_at datetime(6) NOT NULL,
-        updated_at datetime(6) DEFAULT NULL
+    product_id   BINARY(16) PRIMARY KEY,
+    product_name VARCHAR(20) NOT NULL,
+    category     VARCHAR(50) NOT NULL,
+    price        BIGINT      NOT NULL,
+    description  VARCHAR(500) DEFAULT NULL,
+    created_at   datetime(6) NOT NULL,
+    updated_at   datetime(6) DEFAULT NULL
 );
+
+CREATE TABLE orders
+(
+    order_id     BINARY(16) PRIMARY KEY,
+    email        VARCHAR(50)  NOT NULL,
+    address      VARCHAR(200) NOT NULL,
+    postcode     VARCHAR(200) NOT NULL,
+    order_status VARCHAR(50)  NOT NULL,
+    created_at    datetime(6) NOT NULL,
+    updated_at    datetime(6) DEFAULT NULL
+);
+
+CREATE TABLE order_items
+(
+    seq        bigint      NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    order_id   BINARY(16) NOT NULL,
+    product_id BINARY(16) NOT NULL,
+    category   VARCHAR(50) NOT NULL,
+    price      BIGINT      NOT NULL,
+    quantity   int         NOT NULL,
+    created_at datetime(6) NOT NULL,
+    updated_at datetime(6) DEFAULT NULL,
+    INDEX(order_id),
+    CONSTRAINT fk_order_items_to_order FOREIGN KEY (order_id) REFERENCES orders (order_id) ON DELETE CASCADE,
+    CONSTRAINT fk_order_items_to_product FOREIGN KEY (product_id) REFERENCES products (product_id)
+)
